0eb051854d1315eff49e98b2e91e534a5426a7c5
Richard <mugrich007@gmail.com>
2025-11-15 15:14:36 +0000

    auth: implement auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 type AuthInput = { 
     username: string;
@@ -10,9 +11,32 @@ type SigninData = {
     username: string;
 }
 
+type AuthResult = {
+    accessToken: string;
+    userId: number;
+    username: string;
+}
+
 @Injectable()
 export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async authenticate(input: AuthInput): Promise<AuthResult> {
+        const user = await this.validateuser(input)
+        if (!user) throw new UnauthorizedException();
+
+        return {
+            accessToken: 'fake-access',
+            userId: user.userId,
+            username: user.username
+        }
+    }
+
     async validateuser(input: AuthInput): Promise<SigninData | null> {
+        const user = await this.usersService.findUserByName(input.username);
+        if (user && user.password === input.password) {
+            return { userId: user.userId, username: user.username }
+        }
         return null;
     }
 }
